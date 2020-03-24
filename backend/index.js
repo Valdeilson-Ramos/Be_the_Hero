b5ed533a75eb31bafd8e5c39c971ccd45bd5d0c0
valdeilson <vautronica@gmail.com>
2020-03-23 22:31:47 -0300

    add express

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.send("Hello World");
+  return response.json({
+    envento: "Semana omnistack11",
+    aluno: "Valdeilson Ramos"
+  });
 });
 app.listen(3333);
